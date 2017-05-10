e9b0249f090b39d9291ed519a72635da19b525a2
Chess Dean <dingqigd@qq.com>
2017-05-10 10:47:40 +0800

    在MainPage的BottomAppBar中加入了歌曲信息，进度条等控件，重构了MainPage的ContentFrame的跳转方法。

--- MusicPlayer/ViewModels/LocalSongsVM.cs
+++ MusicPlayer/ViewModels/LocalSongsVM.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
 using Windows.Storage.Search;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace MusicPlayer.ViewModels
 {
